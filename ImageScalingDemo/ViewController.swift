df9710329a0bd8d8262e76c2ddec2db6e12fded7
Amit Kumar Singh <amit.aanmol@gmail.com>
2019-04-03 08:49:21 +0530

    - Image scaling

--- ImageScalingDemo/ViewController.swift
+++ ImageScalingDemo/ViewController.swift
@@ -8,13 +8,73 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var chooseButton: UIButton!
+    var imagePicker = UIImagePickerController()
+    
+    @IBAction func btnClicked() {
+        
+        if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum){
+            print("Button capture")
+            
+            imagePicker.delegate = self
+            imagePicker.sourceType = .photoLibrary
+            imagePicker.allowsEditing = false
+            
+            present(imagePicker, animated: true, completion: nil)
+        }
     }
 
-
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if var pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = pickedImage
+            print("Original Image Size: \(pickedImage.size.width) x \(pickedImage.size.height)")
+            pickedImage = scaleImage(fromImage: pickedImage, toNewSize: CGSize(width: 100, height: 100))! // pass image and size of image to scale
+            print("Scaled Image Size: \(pickedImage.size.width) x \(pickedImage.size.height)")
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func scaleImage(fromImage: UIImage, toNewSize: CGSize) -> UIImage? {
+        var newImage: UIImage?
+        let newRect = CGRect(x: 0, y: 0, width: toNewSize.width, height: toNewSize.height).integral
+        UIGraphicsBeginImageContextWithOptions(toNewSize, false, 0)
+        if let context = UIGraphicsGetCurrentContext(), let cgImage = fromImage.cgImage {
+            context.interpolationQuality = .high
+            let flipVertical = CGAffineTransform(a: 1, b: 0, c: 0, d: -1, tx: 0, ty: toNewSize.height)
+            context.concatenate(flipVertical)
+            context.draw(cgImage, in: newRect)
+            if let img = context.makeImage() {
+                newImage = UIImage(cgImage: img)
+            }
+            UIGraphicsEndImageContext()
+        }
+        return newImage
+    }
+    
+    func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
+        let size = image.size
+        
+        let widthRatio  = targetSize.width  / size.width
+        let heightRatio = targetSize.height / size.height
+        
+        var newSize: CGSize
+        if(widthRatio > heightRatio) {
+            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+        } else {
+            newSize = CGSize(width: size.width * widthRatio, height: size.height *      widthRatio)
+        }
+        
+        let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+        
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        image.draw(in: rect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage!
+    }
 }
-
